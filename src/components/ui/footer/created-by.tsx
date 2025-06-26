557f3284e7773ae2b32ee94cdd1e4d4584cb0fc7
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-26 10:32:06 +0330

    fix link issue

--- src/components/ui/footer/created-by.tsx
+++ src/components/ui/footer/created-by.tsx
@@ -1,10 +1,11 @@
 import NextImage from "@/components/ui/image";
+import Link from "next/link";
 
 export default function CreatedByInFooter() {
   return (
-    <div className="w-full bg-white justify-center flex p-4 text-2xl items-center gap-2">
+    <Link href={'https://www.dadekavweb.ir'} target="_blank" className="w-full bg-white justify-center flex p-4 text-2xl items-center gap-2">
       <NextImage url="/images/dadekavweb-logo.webp" alt="dadekavweb-logo" width={57} height={50}/>
       طراحی شده توسط داده کاو وب
-    </div>
+    </Link>
   );
 }
