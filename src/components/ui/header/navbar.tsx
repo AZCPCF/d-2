b1be2487cabf1569177ed8791bcb9a43556ab0ca
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-26 13:04:32 +0330

    Update navbar.tsx

--- src/components/ui/header/navbar.tsx
+++ src/components/ui/header/navbar.tsx
@@ -1,30 +1,26 @@
+// import { CategoryInterface } from "@/interfaces";
 import { FaChevronDown } from "react-icons/fa";
-import { NavbarRequestType } from ".";
 import NextLink from "../link";
-
-const navLinks = [
-  {
-    href: "test",
-    label: (
-      <div className="flex items-center gap-2">
-        <FaChevronDown />
-        <span>محصولات</span>
-      </div>
-    ),
-  },
-  { href: "test", label: "مقالات" },
-  { href: "test", label: "درباره ما" },
-  { href: "test", label: "تماس با ما" },
-];
-
-export default function HeaderNavbar(
-  props: Pick<NavbarRequestType, "categories">
-) {
+// props: {
+//   categories: CategoryInterface[];
+// }
+export default function HeaderNavbar() {
   return (
     <div className="flex gap-10 max-md:hidden">
-      {navLinks.map((link, index) => (
-        <NextLink className="text-xl" key={index} href={link.href} label={link.label} />
-      ))}
+      <div className="">
+        <div className="flex gap-10 max-md:hidden">
+          <div className="group">
+            <div className="flex items-center gap-2 cursor-pointer text-xl hover:text-primary-main">
+              <FaChevronDown />
+              <span>محصولات</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <NextLink className="text-xl" href="/articles" label="مقالات" />
+      <NextLink className="text-xl" href="/about" label="درباره ما" />
+      <NextLink className="text-xl" href="/contact" label="تماس با ما" />
     </div>
   );
 }
