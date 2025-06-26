39ee2a536d3bf5122edb64f514b295f74656c3e6
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-26 13:01:39 +0330

    Update index.ts

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,12 +2,23 @@ export interface CategoryInterface {
   id: number;
   title: string;
   has_sub_category: boolean;
-  parent_id?: string;
+  parent_id?: string | number;
+  sub_categories?: CategoryInterface[];
 }
+
 export interface ImageFromApiInterface {
   width?: number;
   height?: number;
   size?: number;
   url: string;
   alt?: string;
+}
+export interface ProductInterface {
+  id: number;
+  title: string;
+  page_url: string;
+  price: number;
+  after_price: number;
+  discount: number;
+  image_1: ImageFromApiInterface;
 }
