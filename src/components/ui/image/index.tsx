2eb1c46f8359306079d88abe0dbbff017e1c529a
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-26 13:01:44 +0330

    Update index.tsx

--- src/components/ui/image/index.tsx
+++ src/components/ui/image/index.tsx
@@ -1,19 +1,24 @@
+"use client";
 import { ImageFromApiInterface } from "@/interfaces";
+import { cn } from "@/utils/cn";
 import { fileUrl } from "@/utils/env";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function NextImage(
   props: ImageFromApiInterface & { className?: string; alt: string }
 ) {
+  const [src, setSrc] = useState(
+    props.url.startsWith("https") || props.url.startsWith("/images")
+      ? props.url
+      : `${fileUrl}${props.url}`
+  );
   return (
     <Image
-      src={
-        props.url.startsWith("https") || props.url.startsWith("/images")
-          ? props.url
-          : `${fileUrl}${props.url}`
-      }
       {...props}
+      src={src}
       alt={props.alt}
+      onError={() => setSrc("/images/404.png")}
     />
   );
 }
