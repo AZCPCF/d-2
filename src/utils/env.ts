93fed8b29d9e65b1d9588463434d89637471c94d
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-29 02:34:51 +0330

    Update env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,9 +1,10 @@
-export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+export const apiUrlPrimary = process.env.NEXT_PUBLIC_API_URL;
+export const apiUrlSecondary = process.env.NEXT_PUBLIC_API_URL_SECONDARY;
 export const tokenKey = process.env.NEXT_PUBLIC_TOKEN_KEY;
 export const fileUrl = process.env.NEXT_PUBLIC_FILE_URL;
-
 const env = {
-  apiUrl,
+  apiUrlPrimary,
+  apiUrlSecondary,
   tokenKey,
   fileUrl,
 };
