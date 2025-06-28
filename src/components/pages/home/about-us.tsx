a75ce6e49961f2f0888a53b09c9fe7448235985c
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-29 02:41:15 +0330

    use context in home about-us component

--- src/components/pages/home/about-us.tsx
+++ src/components/pages/home/about-us.tsx
@@ -1,24 +1,28 @@
+"use client";
 import NextImage from "@/components/ui/image";
-import { AboutUsRequestInterface } from "@/interfaces/pages/about-us";
+import { useClientCtx } from "@/contexts/client-context";
 import Link from "next/link";
 import { IoIosArrowBack } from "react-icons/io";
 
-export default function HomePageAboutUs({ data }: AboutUsRequestInterface) {
+export default function HomePageAboutUs() {
+  const { aboutUs } = useClientCtx();
   return (
     <section className="grid max-md:grid-cols-1 grid-cols-2 gap-10 px-4 py-12 bg-white max-full mx-auto">
       <div className="overflow-hidden rounded-2xl shadow-md group">
-        <NextImage
-          {...data.poster}
-          alt={data.poster.alt || "درباره ما"}
-          className="w-full h-full object-cover aspect-video transition-transform duration-700 group-hover:scale-105"
-        />
+        {aboutUs?.poster.url ? (
+          <NextImage
+            {...aboutUs?.poster}
+            alt={aboutUs?.poster.alt || "درباره ما"}
+            className="w-full h-full object-cover aspect-video transition-transform duration-700 group-hover:scale-105"
+          />
+        ) : undefined}
       </div>
       <div className="flex flex-col justify-center gap-6 text-right">
         <h1 className="text-4xl font-extrabold text-primary-main leading-tight">
           درباره ما
         </h1>
         <p className="text-xl text-gray-700 leading-relaxed sm:text-2xl">
-          {data.content}
+          {aboutUs?.content}
         </p>
         <div className="mt-6 self-end">
           <Link
@@ -26,7 +30,10 @@ export default function HomePageAboutUs({ data }: AboutUsRequestInterface) {
             className="group inline-flex items-center gap-2 text-primary-main hover:text-primary-dark font-medium text-lg transition-colors"
           >
             مشاهده بیشتر
-            <IoIosArrowBack className="transition-transform duration-300 group-hover:translate-x-1" fontSize={20} />
+            <IoIosArrowBack
+              className="transition-transform duration-300 group-hover:translate-x-1"
+              fontSize={20}
+            />
           </Link>
         </div>
       </div>
