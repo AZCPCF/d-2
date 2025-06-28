398c8edba79393ce99b125a96b91fef90cac1b81
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-29 02:40:10 +0330

    fix href issues

--- src/components/ui/app-bar/index.tsx
+++ src/components/ui/app-bar/index.tsx
@@ -10,9 +10,9 @@ import NextLink from "../link";
 const links = [
   { href: "/", label: "خانه", icon: FaHome },
   { href: "http://localhost:5173", label: "حساب کاربری", icon: BiSolidUser },
-  { href: "/articles", label: "مقالات", icon: PiArticle },
-  { href: "/about", label: "درباره ما", icon: BiInfoCircle },
-  { href: "/contact", label: "تماس با ما", icon: BiPhoneCall },
+  { href: "/articles-hub", label: "مقالات", icon: PiArticle },
+  { href: "/about-us", label: "درباره ما", icon: BiInfoCircle },
+  { href: "/contact-us", label: "تماس با ما", icon: BiPhoneCall },
 ];
 
 export default function AppBar() {
@@ -20,7 +20,7 @@ export default function AppBar() {
 
   return (
     <div className="fixed bottom-4 inset-x-4 z-20 max-md:block hidden">
-      <div className="bg-primary-main/70 backdrop-blur-md shadow-xl rounded-xl flex items-center justify-evenly gap-6 py-3 px-4 border border-white/10">
+      <div className="bg-secondary-main/70 backdrop-blur-md shadow-xl rounded-xl flex items-center justify-evenly gap-6 py-3 px-4 border border-white/10">
         {links.map((link, index) => (
           <NextLink
             key={`app-bar-link-${index}`}
@@ -28,8 +28,8 @@ export default function AppBar() {
             className={cn(
               `flex flex-col items-center text-xs font-medium duration-200`,
               pathname === link.href
-                ? "text-secondary-main"
-                : "text-white hover:text-primary-600"
+                ? "text-primary-main"
+                : "text-white hover:text-secondary-600"
             )}
             label={
               <>
