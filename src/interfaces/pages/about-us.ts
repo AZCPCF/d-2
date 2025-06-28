d35ba10348e924c81e9116493ad091816f3a839f
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-29 02:36:48 +0330

    Update about-us.ts

--- src/interfaces/pages/about-us.ts
+++ src/interfaces/pages/about-us.ts
@@ -1,5 +1,20 @@
 import { ImageFromApiInterface } from "..";
 
+export interface BranchInterface {
+  id: number;
+  title: string;
+  description: string;
+  address: string;
+  phone_number1: string;
+  phone_number2: string;
+  instagram: string;
+  telegram: string;
+  whatsapp: string;
+  image?: ImageFromApiInterface;
+  created_at?: string;
+  updated_at?: string;
+}
+
 export interface AboutUsRequestInterface {
   data: {
     video: string;
@@ -15,4 +30,5 @@ export interface AboutUsRequestInterface {
     telegram: string;
     whatsapp: string;
   };
+  branches: BranchInterface[];
 }
