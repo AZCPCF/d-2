020f17916852e1246ab2c8e2b0707fc709920def
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-28 15:12:15 +0330

    change theme in app-bar

--- src/components/ui/app-bar/index.tsx
+++ src/components/ui/app-bar/index.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import { usePathname } from "next/navigation";
-import NextLink from "../link";
-import { BiInfoCircle, BiPhoneCall } from "react-icons/bi";
-import { PiArticle } from "react-icons/pi";
-import { FaUser } from "react-icons/fa6";
-import { FaHome } from "react-icons/fa";
 import { cn } from "@/utils/cn";
+import { usePathname } from "next/navigation";
+import { BiInfoCircle, BiPhoneCall, BiSolidUser } from "react-icons/bi";
+import { FaHome } from "react-icons/fa";
+import { PiArticle } from "react-icons/pi";
+import NextLink from "../link";
 
 const links = [
   { href: "/", label: "خانه", icon: FaHome },
-  { href: "http://localhost:5173", label: "حساب کاربری", icon: FaUser },
+  { href: "http://localhost:5173", label: "حساب کاربری", icon: BiSolidUser },
   { href: "/articles", label: "مقالات", icon: PiArticle },
   { href: "/about", label: "درباره ما", icon: BiInfoCircle },
   { href: "/contact", label: "تماس با ما", icon: BiPhoneCall },
@@ -21,16 +20,16 @@ export default function AppBar() {
 
   return (
     <div className="fixed bottom-4 inset-x-4 z-20 max-md:block hidden">
-      <div className="bg-secondary-main/60 backdrop-blur-md shadow-xl rounded-xl flex items-center justify-evenly gap-6 py-3 px-4 border border-white/10">
+      <div className="bg-primary-main/70 backdrop-blur-md shadow-xl rounded-xl flex items-center justify-evenly gap-6 py-3 px-4 border border-white/10">
         {links.map((link, index) => (
           <NextLink
             key={`app-bar-link-${index}`}
             href={link.href}
             className={cn(
-              `flex flex-col items-center text-xs font-medium`,
+              `flex flex-col items-center text-xs font-medium duration-200`,
               pathname === link.href
-                ? "text-primary-main"
-                : "text-white hover:text-secondary-800"
+                ? "text-secondary-main"
+                : "text-white hover:text-primary-600"
             )}
             label={
               <>
