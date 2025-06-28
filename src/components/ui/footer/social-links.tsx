490177da3f73889869e8ddb40364803b01932848
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-29 02:39:32 +0330

    use social-links in header-social-links

--- src/components/ui/footer/social-links.tsx
+++ src/components/ui/footer/social-links.tsx
@@ -1,23 +1,10 @@
-import NextLink from "@/components/ui/link";
-import { FaInstagram, FaTelegramPlane, FaWhatsapp } from "react-icons/fa";
-const socialLinks = [
-  { icon: <FaInstagram fontSize={24} />, href: "/" },
-  { icon: <FaWhatsapp fontSize={24} />, href: "/" },
-  { icon: <FaTelegramPlane fontSize={24} />, href: "/" },
-];
-export default function SocialLinks() {
+import SocialLinks from "../social-links";
+export default function SocialLinksHeader() {
   return (
     <div className="w-full flex flex-wrap pb-5">
       <p className="w-full text-end">ما را در شبکه های اجتماعی دنبال کنید</p>
       <div className="w-full flex justify-end items-center pt-3 gap-3">
-        {socialLinks.map((item, i) => (
-          <NextLink
-            className="hover:text-white"
-            key={i}
-            href={item.href}
-            label={item.icon}
-          />
-        ))}
+        <SocialLinks className="hover:text-white" />
       </div>
     </div>
   );
