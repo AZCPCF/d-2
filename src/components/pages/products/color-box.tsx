22d766f927d281f1fc12d919582108a10630b58b
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-29 19:42:33 +0330

    add color-box component

---
+++ src/components/pages/products/color-box.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/utils/cn";
+import { generateDynamicClass } from "@/utils/style-injector";
+import { ReactNode } from "react";
+
+export function ColorBox({
+  color,
+  className,
+  children
+}: {
+  color?: string;
+  className?: string;
+  children?:ReactNode
+}) {
+  const classNameDefault = generateDynamicClass(`background-color: ${color};`);
+
+  return (
+    <div
+      className={cn(
+        className,
+        "w-4 h-4 p-2 shadow-sm border border-black/20 rounded-md",
+        classNameDefault
+      )}
+    >{children}</div>
+  );
+}
