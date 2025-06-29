f7a39b67091b32ed5ad294b55bca66965f1b5543
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-29 13:46:26 +0330

    add gallery page

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,18 @@
+import Galleries from "@/components/pages/gallery";
+import { GalleryRequestInterface } from "@/interfaces/pages/gallery";
+import { fetcher } from "@/lib/fetcher";
+
+export default async function Page() {
+  const res = await fetcher<GalleryRequestInterface>({ endpoint: "galleries" });
+  console.log(res);
+  return (
+    <main className="bg-gray-100 p-10 max-md:p-4">
+      <section className="bg-white p-10 max-md:p-4 rounded-lg grid grid-cols-4 max-md:grid-cols-2 gap-3 shadow-md">
+        <h1 className="text-4xl text-primary-main font-bold col-span-full p-4 text-center">
+          گالری تصاویر D2
+        </h1>
+        <Galleries res={res} />
+      </section>
+    </main>
+  );
+}
---
+++ src/components/pages/gallery/index.tsx
@@ -0,0 +1,48 @@
+"use client";
+import NextImage from "@/components/ui/image";
+import NextModal from "@/components/ui/modal";
+import { GalleryRequestInterface } from "@/interfaces/pages/gallery";
+import { useState } from "react";
+
+export default function Galleries(props: { res: GalleryRequestInterface }) {
+  const [selectedImage, setSelectedImage] = useState<
+    GalleryRequestInterface["data"][0] | null
+  >(null);
+  return (
+    <>
+      {props.res.data.map((item, index) => (
+        <div
+          className="relative w-full overflow-hidden group cursor-pointer"
+          key={`gallery-item-${index}`}
+          onClick={() => setSelectedImage(item)}
+        >
+          <NextImage
+            alt={`تصویر ${index + 1}`}
+            {...item.image}
+            className="w-full h-auto"
+          />
+          <div className="absolute inset-0 bg-black/50 text-white flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity text-center">
+            {item.text}
+          </div>
+        </div>
+      ))}
+
+      {selectedImage && (
+        <NextModal
+          jsx={
+            <>
+              {" "}
+              <NextImage
+                alt={selectedImage.text || `تصویر${selectedImage.id}`}
+                {...selectedImage.image}
+                className="h-auto max-md:max-h-[500px] max-h-[600px] object-contain mb-4"
+              />
+              <p>{selectedImage.text}</p>
+            </>
+          }
+          onClose={() => setSelectedImage(null)}
+        />
+      )}
+    </>
+  );
+}
