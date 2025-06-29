158b054838fade5a1844ba0fe90a11131d291d1f
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-29 16:11:53 +0330

    add categories page

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,9 @@
+import HomePageCategories from "@/components/pages/home/categories";
+
+export default function Categories() {
+  return (
+    <main>
+      <HomePageCategories title="دسته بندی محصولات D2" />
+    </main>
+  );
+}
