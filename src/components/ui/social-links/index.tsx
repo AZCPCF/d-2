3151deffbf699e5ebbfb0543090926cf4e9d462c
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-29 13:48:49 +0330

    use static data in social-links component

--- src/components/ui/social-links/index.tsx
+++ src/components/ui/social-links/index.tsx
@@ -1,23 +1,15 @@
 "use client";
 
 import { useClientCtx } from "@/contexts/client-context";
-import { FaInstagram, FaTelegramPlane, FaWhatsapp } from "react-icons/fa";
+import { socialLinksData } from "@/static";
 import NextLink from "../link";
-import { ReactNode } from "react";
 
-const socialLinks: {
-  icon: ReactNode;
-  href: "instagram" | "whatsapp" | "telegram";
-}[] = [
-  { icon: <FaInstagram fontSize={24} />, href: "instagram" },
-  { icon: <FaWhatsapp fontSize={24} />, href: "whatsapp" },
-  { icon: <FaTelegramPlane fontSize={24} />, href: "telegram" },
-];
+
 const SocialLinks = (props: { className?: string }) => {
   const { aboutUs } = useClientCtx();
   return (
     <>
-      {socialLinks.map((item, index) => (
+      {socialLinksData.map((item, index) => (
         <NextLink
           key={`social-link-${index}`}
           href={aboutUs?.[item.href] || ""}
