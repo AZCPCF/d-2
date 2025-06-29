0277590b494ab4f73dbc1b99ee140dd087a8496f
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-29 13:50:54 +0330

    use static data footer links component

--- src/components/ui/footer/links.tsx
+++ src/components/ui/footer/links.tsx
@@ -1,27 +1,13 @@
 import NextLink from "@/components/ui/link";
+import { footerLinks } from "@/static";
 
-const mainLinks = [
-  { href: "t", label: "اخبار و مقالات" },
-  { href: "about-us", label: "درباره ما" },
-  { href: "contact-us", label: "تماس با ما" },
-  { href: "t", label: "گالری" },
-  { href: "faq", label: "سوالات متداول" },
-];
-
-const otherLinks = [
-  { href: "http://localhost:5173", label: "ورود" },
-  { href: "payment-rules", label: "نحوه ثبت سفارش" },
-  { href: "privacy", label: "حریم خصوصی" },
-  { href: "rules", label: "قوانین سایت" },
-  { href: "licenses", label: "گواهی نامه ها" },
-];
 export default function FooterLinks() {
   return (
     <>
       <div className="col-span-2 max-lg:col-span-6 flex justify-center flex-wrap items-center text-center max-lg:mt-8">
         <h1 className="text-4xl w-full">صفحات</h1>
         <ul className="!text-xl mt-5 flex flex-wrap gap-5 max-sm:gap-2">
-          {mainLinks.map((link, i) => (
+          {footerLinks.main.map((link, i) => (
             <li key={i} className="w-full">
               <NextLink
                 className="hover:text-white w-full list-decimal"
@@ -36,7 +22,7 @@ export default function FooterLinks() {
       <div className="col-span-2 max-lg:col-span-6 flex justify-center flex-wrap items-center text-center max-lg:mt-8">
         <h1 className="text-4xl w-full">سایر</h1>
         <ul className="!text-xl mt-5 flex flex-wrap *:w-full gap-5 max-sm:gap-2">
-          {otherLinks.map((link, i) => (
+          {footerLinks.others.map((link, i) => (
             <li key={i}>
               <NextLink
                 className="hover:text-white"
