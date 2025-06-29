a724338d86f81af0f6a3b858e13bcdb474e7e253
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-29 13:47:17 +0330

    use static data in app-bar

--- src/components/ui/app-bar/index.tsx
+++ src/components/ui/app-bar/index.tsx
@@ -1,35 +1,25 @@
 "use client";
 
+import { appBarlinks } from "@/data";
 import { cn } from "@/utils/cn";
 import { usePathname } from "next/navigation";
-import { BiInfoCircle, BiPhoneCall, BiSolidUser } from "react-icons/bi";
-import { FaHome } from "react-icons/fa";
-import { PiArticle } from "react-icons/pi";
 import NextLink from "../link";
-
-const links = [
-  { href: "/", label: "خانه", icon: FaHome },
-  { href: "http://localhost:5173", label: "حساب کاربری", icon: BiSolidUser },
-  { href: "/articles-hub", label: "مقالات", icon: PiArticle },
-  { href: "/about-us", label: "درباره ما", icon: BiInfoCircle },
-  { href: "/contact-us", label: "تماس با ما", icon: BiPhoneCall },
-];
 
 export default function AppBar() {
   const pathname = usePathname();
 
   return (
     <div className="fixed bottom-4 inset-x-4 z-20 max-md:block hidden">
-      <div className="bg-secondary-main/70 backdrop-blur-md shadow-xl rounded-xl flex items-center justify-evenly gap-6 py-3 px-4 border border-white/10">
-        {links.map((link, index) => (
+      <div className="bg-primary-400/80 backdrop-blur-md shadow-xl rounded-xl flex items-center justify-evenly gap-6 py-3 px-4 border border-white/10">
+        {appBarlinks.map((link, index) => (
           <NextLink
             key={`app-bar-link-${index}`}
             href={link.href}
             className={cn(
               `flex flex-col items-center text-xs font-medium duration-200`,
               pathname === link.href
-                ? "text-primary-main"
-                : "text-white hover:text-secondary-600"
+                ? "text-secondary-main hover:text-secondary-main"
+                : "text-white hover:text-secondary-main"
             )}
             label={
               <>
