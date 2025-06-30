484c2b8f03d443d5f55adb9b9ed521e37ef3f1f7
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-30 15:28:40 +0330

    add incredible-offers page

---
+++ src/app/incredible-offers/page.tsx
@@ -0,0 +1,27 @@
+import ProductCard from "@/components/pages/products/card";
+import { GetByCategoryRequestInterface } from "@/interfaces/pages/category";
+import { fetcher } from "@/lib/fetcher";
+
+export default async function IncredibleOffers() {
+  const res = await fetcher<GetByCategoryRequestInterface>({
+    endpoint: "products",
+  });
+  return (
+    <main className="bg-gray-100 p-10 max-sm:p-4">
+      <section className="bg-gray-50 p-10 gird grid-cols-1 shadow-md rounded-lg max-sm:p-4">
+        <h1 className="text-4xl font-bold text-center text-primary-main p-4 mb-4">
+          {"پیشنهاد های شگفت انگیز"}
+        </h1>
+        <div className="grid grid-cols-6 gap-5 max-xl:grid-cols-5 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2">
+          {res.data.map((product, index) => (
+            <ProductCard
+              key={`categories-product-item-${index}`}
+              product={product}
+              showColors
+            />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
