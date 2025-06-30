af8b113c4972d08b4b4570f17a1f62d0e9df8e50
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-30 09:07:23 +0330

    chnage app-bar color

--- src/components/ui/app-bar/index.tsx
+++ src/components/ui/app-bar/index.tsx
@@ -10,7 +10,7 @@ export default function AppBar() {
 
   return (
     <div className="fixed bottom-4 inset-x-4 z-20 max-md:block hidden">
-      <div className="bg-primary-400/80 backdrop-blur-md shadow-xl rounded-xl flex items-center justify-evenly gap-6 py-3 px-4 border border-white/10">
+      <div className="bg-secondary-400/80 backdrop-blur-md shadow-xl rounded-xl flex items-center justify-evenly gap-6 py-3 px-4 border border-white/10">
         {appBarlinks.map((link, index) => (
           <NextLink
             key={`app-bar-link-${index}`}
@@ -18,8 +18,8 @@ export default function AppBar() {
             className={cn(
               `flex flex-col items-center text-xs font-medium duration-200`,
               pathname === link.href
-                ? "text-secondary-main hover:text-secondary-main"
-                : "text-white hover:text-secondary-main"
+                ? "text-primary-main hover:text-primary-main"
+                : "text-white hover:text-primary-main"
             )}
             label={
               <>
