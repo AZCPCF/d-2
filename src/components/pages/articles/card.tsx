2d0e02840cf1cce7523565b1091c9f452f5c8e05
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-30 20:21:04 +0330

    add articles page

---
+++ src/components/pages/articles/card.tsx
@@ -0,0 +1,32 @@
+import NextImage from "@/components/ui/image";
+import { ArticleInterface } from "@/interfaces";
+import Link from "next/link";
+
+export default function ArticleCard({
+  article,
+}: {
+  article: ArticleInterface;
+}) {
+  return (
+    <Link
+      href={`/article/${article.page_url}`}
+      className="group block overflow-hidden relative rounded-lg bg-white"
+    >
+      <div className="relative h-48 w-full overflow-hidden">
+        <NextImage
+          {...article.main_image}
+          alt={article.main_image.alt || article.title}
+          className="w-full h-full group-hover:scale-105 transition-transform duration-300"
+        />
+      </div>
+      <div className="p-4 h-36 relative bg-white">
+        <h2 className="text-lg font-bold text-gray-800 group-hover:text-primary-main transition-colors duration-200 line-clamp-2">
+          {article.title}
+        </h2>
+        <p className="text-sm text-gray-500 absolute bottom-2 left-2">
+          {article.date.str || article.date.string}
+        </p>
+      </div>
+    </Link>
+  );
+}
