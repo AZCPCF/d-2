764b3182c5a54f82555798a248cfa9a04d895773
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-30 16:23:54 +0330

    use toltip

--- src/components/pages/products/color-box.tsx
+++ src/components/pages/products/color-box.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Tooltip from "@/components/ui/tooltip";
 import { cn } from "@/utils/cn";
 import isDarkColor from "@/utils/color-checker";
 import { generateDynamicClass } from "@/utils/style-injector";
@@ -16,21 +17,13 @@ export function ColorBox({
 }) {
   const classNameDefault = generateDynamicClass(`background-color: ${color};`);
   const isDark = isDarkColor(color);
-  const tooltipTextClass = isDark ? "text-white" : "text-black";
 
   return (
     <div className="relative inline-block group">
-      {title && (
-        <div
-          className={cn(
-            "absolute bottom-full mb-1 left-1/2 -translate-x-1/2 scale-0 group-hover:scale-100 transition-transform bg-secondary-main text-lg max-md:text-xs px-2 py-1 rounded shadow-lg z-10 whitespace-nowrap",
-            tooltipTextClass,
-            classNameDefault
-          )}
-        >
-          {title}
-        </div>
-      )}
+      <Tooltip
+        className={cn(classNameDefault,isDark?"text-white":"text-black")}
+        title={title}
+      />
       <div
         className={cn(
           "w-4 h-4 p-2 shadow-sm border border-black/20 rounded-md",
