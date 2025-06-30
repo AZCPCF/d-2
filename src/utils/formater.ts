ad67d148e30925a591aeddeec81d444e28d998c5
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-30 15:29:17 +0330

    remove tostring in formater util

--- src/utils/formater.ts
+++ src/utils/formater.ts
@@ -1,3 +1,3 @@
 export function formatNumberWithCommas(num: number | string): string {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return `${num}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
