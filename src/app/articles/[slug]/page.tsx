19c7399ab7da65e97dcb0c3bdf37e41199575b4a
Alisan <99.a.cpcf.r.99@gmail.com>
2025-06-30 20:21:39 +0330

    add articles/[slug] page

---
+++ src/app/articles/[slug]/page.tsx
@@ -0,0 +1,43 @@
+import ArticleCard from "@/components/pages/articles/card";
+import { ArticleInterface } from "@/interfaces";
+import { fetcher } from "@/lib/fetcher";
+
+export default async function ArticlesSlug({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const res = await fetcher<{
+    data: ArticleInterface[];
+    category: string;
+  }>({
+    endpoint: "articles_filter",
+    params: {
+      page_url: slug,
+    },
+  });
+  return (
+    <main className="bg-gray-200/75 p-10 max-sm:p-4 flex flex-col gap-10">
+      <section
+        className={`bg-gray-50 p-10 grid grid-cols-1 shadow-md rounded-lg max-sm:p-4`}
+      >
+        <h1
+          className={`text-3xl sm:text-4xl font-bold text-center text-primary-main p-4 tracking-wide`}
+        >
+          {res.category}
+        </h1>
+        <div className="grid grid-cols-6 gap-5 max-xl:grid-cols-5 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2">
+          {res.data.map((article, index) => (
+            <div
+              className="rounded-lg shadow-md hover:shadow-xl transform hover:-translate-y-1 transition duration-300"
+              key={`articles-hub-item-${index}`}
+            >
+              <ArticleCard article={article} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
