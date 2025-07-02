61d6b73065244624e79d9f8fad50a698480b7228
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-02 19:18:54 +0330

    Create loading.tsx

---
+++ src/app/rules/loading.tsx
@@ -0,0 +1,6 @@
+import RulesPageLoading from "@/components/loading/rules";
+import { VscLaw } from "react-icons/vsc";
+
+export default function RulesLoading() {
+  return <RulesPageLoading icon={<VscLaw />} title="قوانین سایت" />;
+}
