bca3a2cecaee9e142cf2bb4fbf3dee402aa74938
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-02 19:19:03 +0330

    Create loading.tsx

---
+++ src/app/privacy/loading.tsx
@@ -0,0 +1,6 @@
+import RulesPageLoading from "@/components/loading/rules";
+import { BiInfoCircle } from "react-icons/bi";
+
+export default function PrivacyLoading() {
+  return <RulesPageLoading icon={<BiInfoCircle />} title="حریم خصوصی D2" />;
+}
