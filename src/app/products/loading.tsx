34052034c377348edc2eaa5f623ca93769beab3e
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-02 19:18:56 +0330

    Create loading.tsx

---
+++ src/app/products/loading.tsx
@@ -0,0 +1,9 @@
+import HomeCategoriesSkeleton from "@/components/loading/home/categories";
+
+export default function ProductsLoading() {
+  return (
+    <main>
+      <HomeCategoriesSkeleton title="دسته بندی محصولات D2" />
+    </main>
+  );
+}
