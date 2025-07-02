deebc684635f8c4bfe7b213787da57e2bac80948
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-02 19:18:35 +0330

    Create article-card.tsx

---
+++ src/components/loading/ui/article-card.tsx
@@ -0,0 +1,12 @@
+export default function ArticleCardSkeleton() {
+  return (
+    <div className="min-w-[250px] max-w-[250px] rounded-lg shadow-md bg-white animate-pulse overflow-hidden">
+      <div className="h-52 w-full bg-gray-300" />
+      <div className="p-4 h-36 relative">
+        <div className="h-4 bg-gray-300 rounded w-5/6 mb-2" />
+        <div className="h-4 bg-gray-200 rounded w-3/4 mb-6" />
+        <div className="h-3 bg-gray-300 rounded w-1/3 absolute bottom-2 left-2" />
+      </div>
+    </div>
+  );
+}
