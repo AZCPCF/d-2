8e2415732ad247d5537355c79409bf6b627d6521
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-02 19:20:15 +0330

    Create loading.tsx

---
+++ src/app/incredible-offers/loading.tsx
@@ -0,0 +1,5 @@
+import CategoryPageLoading from "@/components/loading/category";
+
+export default function IncredibleOffersLoading() {
+  return <CategoryPageLoading title="پیشنهاد های شگفت انگیز" />;
+}
