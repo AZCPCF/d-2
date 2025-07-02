d22088722ab6748cc77e91703d777d13b7f9fc91
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-02 19:18:32 +0330

    Update index.tsx

--- src/components/pages/faq/index.tsx
+++ src/components/pages/faq/index.tsx
@@ -4,16 +4,16 @@ export default function Faqs(props: { res: FaqRequestInterface }) {
   return (
     <ul className="space-y-6">
       {props.res.data.map((item, index) => (
-        <li key={`faq-item${index}`}>
+        <li key={`faq-item-${index}`}>
           <div className="group">
             <details
-              className="group border rounded-md p-4 transition-colors 
+              className="group border rounded-md transition-colors 
                  group-[.group]:open:border-primary-main border-gray-300"
             >
-              <summary className="font-medium text-xl cursor-pointer group-open:text-primary-main max-md:text-lg">
+              <summary className="font-medium text-xl p-4 cursor-pointer group-open:text-primary-main max-md:text-lg">
                 {item.question}
               </summary>
-              <div className="mt-2 text-secondary-main text-lg max-md:text-base">
+              <div className="mt-2 text-secondary-main px-4 py-2 text-lg max-md:text-base">
                 {item.answer}
               </div>
             </details>
