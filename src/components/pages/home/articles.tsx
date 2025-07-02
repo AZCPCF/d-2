fddf586f7cf32ce26f7548223631002dc1c0373a
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-02 19:18:31 +0330

    Update articles.tsx

--- src/components/pages/home/articles.tsx
+++ src/components/pages/home/articles.tsx
@@ -5,6 +5,7 @@ import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
 import { IoIosArrowBack } from "react-icons/io";
 import ArticleCard from "../articles/card";
+import Link from "next/link";
 
 export default function HomePageArticles({
   articles,
@@ -24,7 +25,7 @@ export default function HomePageArticles({
     <section className="px-4 py-16 mx-auto w-full">
       <h1 className="flex items-center justify-between flex-wrap gap-4 text-4xl font-extrabold text-primary-main m-10 max-md:m-4 relative">
         <span>جدیدترین مقالات</span>
-        <a
+        <Link
           href={"/articles-hub"}
           className="text-xl max-md:text-base group inline-flex items-center gap-2 font-semibold text-primary-main rounded px-4 py-1 transition max-md:w-full max-md:flex justify-end max-md:p-0 mt-4"
         >
@@ -32,7 +33,7 @@ export default function HomePageArticles({
           <span className="opacity-0 translate-x-[-4px] group-hover:opacity-100 group-hover:translate-x-0 transition duration-300 max-md:opacity-100">
             <IoIosArrowBack className="text-[32px] max-md:text-[24px]" />
           </span>
-        </a>
+        </Link>
       </h1>
 
       <div ref={sliderRef} className="keen-slider py-6">
