e7d0916fd8b9df93fa0e5c82a23b93de31d79b03
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-03 13:58:51 +0330

    Delete loading.tsx

--- src/app/categories/loading.tsx
+++
@@ -1,9 +0,0 @@
-import HomeCategoriesSkeleton from "@/components/loading/home/categories";
-
-export default function CategoriesLoading() {
-  return (
-    <main>
-      <HomeCategoriesSkeleton title="دسته بندی محصولات D2" />
-    </main>
-  );
-}
