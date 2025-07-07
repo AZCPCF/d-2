95472995152ad9ec982175198c9bcb8388cdcb1d
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-07 16:58:03 +0330

    Update description.tsx

--- src/components/ui/footer/description.tsx
+++ src/components/ui/footer/description.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { useClientCtx } from "@/contexts/client-context";
 import { parser } from "@/utils/parser";
+import { useMemo } from "react";
 
 export default function FooterDescription() {
   const { aboutUs } = useClientCtx();
-  const { ParsedNode } = parser(aboutUs?.content || "");
+  const content = aboutUs?.content || "";
+
+  const { ParsedNode } = useMemo(() => parser(content), [content]);
+
   return (
     <div className="line-clamp-[7] max-md:line-clamp-none overflow-hidden w-full">
       <ParsedNode className="p-0" />
