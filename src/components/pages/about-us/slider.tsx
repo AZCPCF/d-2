c379087b13133389ea85dc65e8069aedc8a46cd3
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-07 16:58:16 +0330

    Update slider.tsx

--- src/components/pages/about-us/slider.tsx
+++ src/components/pages/about-us/slider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import NextImage from "@/components/ui/image";
+import MediaPlayer from "@/components/ui/media-player";
 import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
 
@@ -8,14 +9,12 @@ interface AboutMediaSliderProps {
   images: { url: string }[];
   video: string;
   poster: { url: string };
-  videoText: string;
 }
 
 export default function AboutMediaSlider({
   images,
-  // video,
-  // poster,
-  // videoText,
+  video,
+  poster,
 }: AboutMediaSliderProps) {
   const [sliderRef] = useKeenSlider({
     loop: true,
@@ -23,14 +22,10 @@ export default function AboutMediaSlider({
   });
   return (
     <section className="w-full max-w-4xl mx-auto mb-">
-      <h2 className="text-center text-2xl font-bold text-primary-main mb-6">
-        {/* {videoText} */}
-      </h2>
       <div ref={sliderRef} className="keen-slider rounded-lg overflow-hidden">
-        {/* <div className="keen-slider__slide relative w-full h-full"> */}
-         {/* <MediaPlayer poster={poster} src={video}/> */}
-        {/* </div> */}
-
+        <div className="keen-slider__slide relative w-full h-full">
+          <MediaPlayer poster={poster} src={video} />
+        </div>
         {images.map((img, idx) => (
           <div key={idx} className="keen-slider__slide relative w-full h-full">
             <NextImage alt={`D2 media ${idx + 1}`} {...img} />
