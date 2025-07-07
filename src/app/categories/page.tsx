706fde4d27998743e24b073f770034fa09018674
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-07 16:59:03 +0330

    Update page.tsx

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,5 +1,14 @@
 import HomePageCategories from "@/components/pages/home/categories";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "دسته بندی محصولات | فروشگاه پوشاک دی دو",
+  keywords: [
+    "دسته بندی محصولات مردانه",
+    "دسته بندی محصولات بچگانه",
+    "فروشگاه اینترنتی پوشاک",
+    "فروشگاه پوشاک دی دو",
+  ],
+};
 export default function Categories() {
   return (
     <main>
