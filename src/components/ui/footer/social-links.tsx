009c037840a3be8881a5461a4f681a0f9d3ccda4
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-07 16:57:55 +0330

    Update social-links.tsx

--- src/components/ui/footer/social-links.tsx
+++ src/components/ui/footer/social-links.tsx
@@ -1,8 +1,12 @@
 import SocialLinks from "../social-links";
+
 export default function SocialLinksHeader() {
   return (
     <div className="w-full flex flex-wrap pb-5">
+      {/* Header text */}
       <p className="w-full text-end">ما را در شبکه های اجتماعی دنبال کنید</p>
+
+      {/* Social links aligned to the right */}
       <div className="w-full flex justify-end items-center pt-3 gap-3">
         <SocialLinks className="hover:text-primary-800" />
       </div>
