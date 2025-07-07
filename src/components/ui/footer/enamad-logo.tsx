b961d1b0912effaec3100672f2e32dd680be12d9
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-07 16:58:00 +0330

    Update enamad-logo.tsx

--- src/components/ui/footer/enamad-logo.tsx
+++ src/components/ui/footer/enamad-logo.tsx
@@ -2,19 +2,18 @@ import NextImage from "@/components/ui/image";
 import Link from "next/link";
 
 export default function EnamadLogo() {
+  const enamadUrl = "https://trustseal.enamad.ir/?id=527519&Code=rob4yxOTAgWtwcl4QWQ1lXSxyRO3CRxz";
+  const enamadLogoUrl = "https://trustseal.enamad.ir/logo.aspx?id=527519&Code=rob4yxOTAgWtwcl4QWQ1lXSxyRO3CRxz";
+
   return (
     <Link
-      href={
-        "https://trustseal.enamad.ir/?id=527519&Code=rob4yxOTAgWtwcl4QWQ1lXSxyRO3CRxz"
-      }
+      href={enamadUrl}
       target="_blank"
       rel="noopener noreferrer"
       className="w-full flex justify-center items-center py-20 max-lg:py-10"
     >
       <NextImage
-        url={
-          "https://trustseal.enamad.ir/logo.aspx?id=527519&Code=rob4yxOTAgWtwcl4QWQ1lXSxyRO3CRxz"
-        }
+        url={enamadLogoUrl}
         alt="enamad-logo"
         className="w-[100px]"
         width={100}
