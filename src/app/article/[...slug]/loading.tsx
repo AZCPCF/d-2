0ec4e760a7fbdb535368f086ffda2f7903ad2097
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-08 14:04:12 +0330

    Update loading.tsx

--- src/app/article/[...slug]/loading.tsx
+++ src/app/article/[...slug]/loading.tsx
@@ -3,7 +3,7 @@ export default function ArticleLoading() {
     <main className="bg-gray-200/75 py-10 px-8 max-sm:px-4">
       <section className="grid-cols-12 grid gap-4 animate-pulse">
         {/* Main Content */}
-        <div className="col-span-8 max-md:col-span-full rounded-xl grid grid-cols-1 bg-white relative shadow-md">
+        <div className="col-span-8 max-md:col-span-full rounded-xl grid grid-cols-1 bg-background relative shadow-md">
           <div className="w-full flex items-center justify-center px-6 pt-6 mt-6">
             <div className="h-8 w-2/3 bg-gray-300 rounded mt-8" />
             <div className="absolute left-2 top-2 flex gap-2">
@@ -25,7 +25,7 @@ export default function ArticleLoading() {
 
         {/* Sidebar */}
         <div className="col-span-4 max-md:col-span-full">
-          <div className="sticky top-4 bg-white rounded-xl shadow-md p-4 space-y-4">
+          <div className="sticky top-4 bg-background rounded-xl shadow-md p-4 space-y-4">
             <div className="h-6 w-1/3 bg-gray-300 rounded" />
 
             {Array.from({ length: 3 }).map((_, i) => (
