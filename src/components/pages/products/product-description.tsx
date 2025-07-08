0fd705202e8fea88824a0867bb1b6bb579d92868
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-08 13:48:42 +0330

    Update product-description.tsx

--- src/components/pages/products/product-description.tsx
+++ src/components/pages/products/product-description.tsx
@@ -5,7 +5,7 @@ import { ProductInterface } from "@/interfaces";
 export default function ProductDescription({ res }: { res: ProductInterface }) {
   const { ParsedNode } = parser(res.description);
   return (
-    <div className="col-span-4 max-md:col-span-12 max-lg:col-span-full max-lg:order-10">
+    <div className="col-span-4 max-md:col-span-12 max-lg:col-span-full max-lg:order-10 dark:text-white">
       <ParsedNode />
       <ProductComments productId={res.id} comments={res.comments} />
     </div>
