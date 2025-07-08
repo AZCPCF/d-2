40f45c4b709c4d9b234bff29a78bc9e5d2d283e5
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-08 14:24:36 +0330

    Update loading.tsx

--- src/app/gallery/loading.tsx
+++ src/app/gallery/loading.tsx
@@ -1,6 +1,6 @@
 export default function GalleryLoading() {
   return (
-    <main className="bg-gray-100 p-10 max-md:p-4 animate-pulse">
+    <main className="bg-gray-100 p-10 max-md:p-4 loading">
       <section className="bg-background p-10 max-md:p-4 rounded-lg grid grid-cols-4 max-lg:grid-cols-3 max-md:grid-cols-2 gap-3 shadow-md">
         <h1 className="text-4xl text-primary-main font-bold col-span-full p-4 text-center">
           گالری تصاویر D2
