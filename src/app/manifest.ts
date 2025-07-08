630961c42ee14f530866cbc23bbdecac48cfb2ae
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-08 17:37:46 +0330

    use screenshots in manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,37 +2,69 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
+    id: "azcpcf-d2-collection",
+    dir: "rtl",
+    lang: "fa",
     name: "D2",
     short_name: "D2 Collection",
     description: "فروشگاه پوشاک مردانه و بچگانه",
     start_url: "/",
+    scope: "/",
     display: "standalone",
+    display_override: ["standalone", "fullscreen", "window-controls-overlay"],
+    orientation: "any",
     background_color: "#ffffff",
     theme_color: "#fed00b",
+    prefer_related_applications: false,
+    launch_handler: {
+      client_mode: "auto",
+    },
+    categories: ["shopping"],
     icons: [
       {
         src: "/icon-192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
       },
       {
         src: "/icon-512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
-    categories: ["shopping"],
-    dir: "rtl",
-    id: "azcpcf-d2-collection",
-    lang: "fa",
-    shortcuts: [
-      { name: "درباره ما", url: "/about-us" },
-      { name: "ارتباط با ما", url: "/contact-us" },
-      { name: "دسته بندی ها", url: "/categories" },
+    screenshots: [
+      { src: "/screenshots/windows-ss.png", platform: "windows" },
+      { src: "/screenshots/android-ss.png", platform: "android" },
+      { src: "/screenshots/windows-ss-dark.png", platform: "windows" },
+      { src: "/screenshots/android-ss-dark.png", platform: "android" },
     ],
-    launch_handler: { client_mode: "auto" },
-    orientation: "any",
-    display_override: ["standalone", "fullscreen"],
-    scope: "/",
+    shortcuts: [
+      {
+        name: "درباره ما",
+        url: "/about-us",
+      },
+      {
+        name: "ارتباط با ما",
+        url: "/contact-us",
+      },
+      {
+        name: "دسته بندی ها",
+        url: "/categories",
+      },
+    ],
   };
 }
