50f79507a47386220539983fc358458e65c8a826
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-08 13:49:22 +0330

    Update page.tsx

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -19,7 +19,7 @@ export default async function Page() {
   const res = await fetcher<GalleryRequestInterface>({ endpoint: "galleries" });
   return (
     <main className="bg-gray-100 p-10 max-md:p-4">
-      <section className="bg-white p-10 max-md:p-4 rounded-lg grid grid-cols-4 max-md:grid-cols-2 gap-3 shadow-md">
+      <section className="bg-background p-10 max-md:p-4 rounded-lg grid grid-cols-4 max-md:grid-cols-2 gap-3 shadow-md">
         <h1 className="text-4xl text-primary-main font-bold col-span-full p-4 text-center">
           گالری تصاویر D2
         </h1>
