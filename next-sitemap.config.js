8e5abe87826db1e6ff37d654d2591c080c56aadd
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-08 18:53:14 +0330

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://d-2-orpin.vercel.app",
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  priority: 0.7,
+};
