2c9b785b0b9db0e8ebd397360d76179986b52a97
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-08 13:49:28 +0330

    Update error.tsx

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import Link from 'next/link';
-import { FaBug } from 'react-icons/fa';
+import { useEffect } from "react";
+import Link from "next/link";
+import { FaBug } from "react-icons/fa";
 
 export default function GlobalError({
   error,
@@ -12,22 +12,25 @@ export default function GlobalError({
   reset: () => void;
 }) {
   useEffect(() => {
-    console.error('Captured error:', error);
+    console.error("Captured error:", error);
   }, [error]);
 
   return (
     <html>
       <body>
-        <main className='home'>
-          <section className="flex flex-col items-center justify-center my-40 text-right px-4">
+        <main className="home">
+          <section className="flex flex-col items-center justify-center py-40 text-right px-4">
             <h1 className="font-extrabold text-red-600 mb-4 flex gap-2 items-center">
               <FaBug fontSize={48} />
               <FaBug fontSize={48} />
-              <span className='text-8xl'>5</span>
+              <span className="text-8xl">5</span>
             </h1>
-            <h2 className="text-3xl text-gray-900 mb-2">خطایی رخ داده است</h2>
-            <p className="text-gray-600 mb-6 text-lg max-[500px]:text-sm">
-              مشکلی در پردازش درخواست به وجود آمده. لطفاً دوباره تلاش کنید یا با پشتیبانی تماس بگیرید.
+            <h2 className="text-3xl text-gray-900 mb-2 dark:text-red-700">
+              خطایی رخ داده است
+            </h2>
+            <p className="text-gray-600 mb-6 text-lg max-[500px]:text-sm dark:text-white">
+              مشکلی در پردازش درخواست به وجود آمده. لطفاً دوباره تلاش کنید یا با
+              پشتیبانی تماس بگیرید.
             </p>
             <div className="flex gap-4">
               <button
