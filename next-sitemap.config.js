5a5d7dbd3a2807234826bddf105f0d4f0efa6330
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-09 18:41:13 +0330

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   siteUrl: "https://azcpcf-d-2.vercel.app",
   generateRobotsTxt: true,
+  exclude: ["/secret", "/admin/*"],
+  sitemapSize: 5000,
   changefreq: "weekly",
   priority: 0.7,
+  transform: async (config, path) => {
+    // Custom logic per path if needed
+    return {
+      loc: path, // => this will be prepended by siteUrl
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: new Date().toISOString(),
+    };
+  },
 };
