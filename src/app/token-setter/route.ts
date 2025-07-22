631d310b865f3391e42aa5eb0b2569d6ade99b0c
Alisan <99.a.cpcf.r.99@gmail.com>
2025-07-22 19:00:17 +0330

    add token-setter route

---
+++ src/app/token-setter/route.ts
@@ -0,0 +1,37 @@
+import { appUrl, tokenKey } from "@/utils/env";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request): Promise<Response> {
+  try {
+    const body = (await request.json()) as { token?: string };
+    const { token } = body;
+
+    if (!token) {
+      return new Response(JSON.stringify({ error: "Token is required" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    const nextCookies = await cookies();
+
+    nextCookies.set({
+      name: tokenKey!,
+      value: token,
+      maxAge: 60 * 60 * 24 * 365,
+    });
+
+    return new Response(
+      JSON.stringify({ message: "Token received and set", appUrl }),
+      {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: "Invalid JSON" }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
